70bcf329fc6cd1df469f7ab9c3039709876fad72
LibrazionPc <znwajei@gmail.com>
2024-10-21 00:35:20 +0100

    More req

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,6 +3,7 @@
 0-minoperations.py
 """
 
+
 def minOperations(n):
     """Minimum operations algorithm"""
     if n <= 1:
@@ -12,6 +13,6 @@ def minOperations(n):
 
     for i in range(2, n + 1):
         while n % i == 0:
-            operations += i  # Add i for "Copy All" + "Paste"
-            n //= i          # Reduce n
-    return operations
+            operations += i
+            n //= i
+            return operations
