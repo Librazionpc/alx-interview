722bb966273b80a4c17c072b13e0fcdf9ed40e5b
LibrazionPc <znwajei@gmail.com>
2024-10-31 17:29:30 +0100

    utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+0-validate_utf8.py
+"""
+
+def validUTF8(data):
+    """Determines if a given data set represents a valid UTF-8 encoding"""
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while mask & byte:
+                num_bytes += 1
+                mask = mask >> 1
+            if num_bytes == 0:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (byte & 1 << 7 and not byte & 1 << 6):
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+Dealing With uft-8
