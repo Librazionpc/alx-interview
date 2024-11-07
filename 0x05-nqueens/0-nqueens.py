30b326e83b9bfce8c027b1d6391cd0ad3b406b2c
LibrazionPc <znwajei@gmail.com>
2024-11-07 23:05:12 +0100

    N-queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import sys
+"""N-queens problem."""
+
+
+def is_valid(board, row, col):
+    """Check if a queen can be placed at board[row][col]."""
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+def solve_nqueens(n, row, board, solutions):
+    """Use backtracking to find all solutions for n-queens."""
+    if row == n:
+        solution = [[i, board[i]] for i in range(n)]
+        solutions.append(solution)
+        return
+
+    for col in range(n):
+        if is_valid(board, row, col):
+            board[row] = col
+            solve_nqueens(n, row + 1, board, solutions)
+            board[row] = -1
+
+def main():
+    """Parse command line arguments """
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = []
+    board = [-1] * n
+    solve_nqueens(n, 0, board, solutions)
+
+    for solution in solutions:
+        print(solution)
+
+if __name__ == "__main__":
+    main()
